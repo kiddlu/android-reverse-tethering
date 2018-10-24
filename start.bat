a95dcb097d71172f8abee7c70f3aab4be14f285a
kiddlu <kidd.dawny.lu@gmail.com>
2018-10-24 13:39:23 +0800

    init repo
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo on
+
+::notes use the same adb with Vysor, if localhost not work, use 127.0.0.1
+adb.exe reverse tcp:8388 tcp:18630
+%~dp0\shadowsocks-libqss.exe -c %~dp0\config_server.json -S
